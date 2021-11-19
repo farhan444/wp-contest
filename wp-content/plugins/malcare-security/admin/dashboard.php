18b7f6e292766c60f77234183d294ba292abb98f
farhan444 <farhanayub444@gmail.com>
2021-11-19 16:02:05 +0000

    commit

--- wp-content/plugins/malcare-security/admin/dashboard.php
+++
@@ -1,4 +0,0 @@
-<div>
-	<iframe style="width: 100%; height: 1900px; padding-top: 13px;"src="<?=$this->account->authenticatedUrl('/malcare/access')?>">
-	</iframe>
-</div>
--- wp-content/plugins/malcare-security/protect/prepend/ignitor.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined('MCDATAPATH')) exit;
-
-if (defined('MCCONFKEY')) {
-	require_once dirname( __FILE__ ) . '/protect.php';
-
-	$mcProtect = new BVPrependProtect();
-	$mcProtect->run();
-}
